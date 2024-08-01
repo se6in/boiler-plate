24b94d2c0cd21cdb62ded84e22507a2c6d360be1
sebin <sebin2752@naver.com>
2024-08-01 18:47:55 +0900

    로그인, 토큰완료

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 const{User} = require("./models/User");
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -36,6 +38,44 @@ app.post('/register', (req, res) => {
   })
 })
 
+app.post('/login', (req, res) => {
+  // 이메일이 DB에 있는지 확인
+  User.findOne({
+      email: req.body.email
+  })
+  .then (async (user) => {
+      if (!user) {
+          throw new Error("제공된 이메일에 해당하는 유저가 없습니다.")
+      }
+      // 비밀번호가 일치하는지 확인
+      const isMatch = await user.comparePassword(req.body.password);
+      return { isMatch, user };
+  })
+  .then(({ isMatch, user }) => {
+      console.log(isMatch);
+      if (!isMatch) {
+          throw new Error("비밀번호가 틀렸습니다.")
+      }
+      // 로그인 완료
+      return user.generateToken();
+  })
+  .then ((user) => {
+      // 토큰 저장 (쿠키, localstorage ...)
+      return res.cookie("x_auth", user.token)
+      .status(200)
+      .json({
+          loginSuccess: true,
+          userId: user._id
+      });
+  })
+  .catch ((err) => {
+      console.log(err);
+      return res.status(400).json({
+          loginSuccess: false,
+          message: err.message
+      });
+  })
+});
 
 
 app.listen(port, () => {
